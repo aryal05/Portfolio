eb2b51f3a6c6406e42bd153b25b558638163277c
Ami <aryal.rajat05@gmail.com>
2024-05-25 20:25:52 +0545

    portfolio Completed

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-var typed = new Typed(".multiple-text",{
+var  typed = new Typed(".multiple-text",{
      strings:["Full Stack Developer", "Creater", "Youtuber"],
      typeSpeed:100,
      backSpeed:100,
@@ -6,3 +6,22 @@ var typed = new Typed(".multiple-text",{
      loop:true
 
 })
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+     anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          const targetId = this.getAttribute('href');
+          console.log('Target ID:', targetId); // Debug log
+          const targetElement = document.querySelector(targetId);
+          if (targetElement) {
+               console.log('Target Element:', targetElement); // Debug log
+               targetElement.scrollIntoView({
+                    behavior: 'smooth'
+               });
+          } else {
+               console.error('Element not found for ID:', targetId); // Error log
+          }
+     });
+});
